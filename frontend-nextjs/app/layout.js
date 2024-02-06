c956f55cde4e44b299cc85ef91b34b1001701cfd
Lokesh Pereiro <lpdraft001@gmail.com>
2024-02-06 23:58:41 +0100

    add to cart done, few bugs to be handeled

---
+++ frontend-nextjs/app/layout.js
@@ -0,0 +1,33 @@
+"use client";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "./_components/Header";
+import Footer from "./_components/Footer";
+import { ClerkProvider } from "@clerk/nextjs";
+import { CartContext } from "./_context/CartContext";
+import { useState } from "react";
+
+const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata = {
+//   title: "CalitosBoss",
+//   description: "Find or sell your old/new best tennis rackets",
+// };
+
+export default function RootLayout({ children }) {
+  const [cart, setCart] = useState([]);
+
+  return (
+    <ClerkProvider>
+      <CartContext.Provider value={{ cart, setCart }}>
+        <html lang="en">
+          <body className={inter.className}>
+            <Header />
+            {children}
+            <Footer />
+          </body>
+        </html>
+      </CartContext.Provider>
+    </ClerkProvider>
+  );
+}
---
+++ frontend-nextjs/app/page.js
@@ -0,0 +1,10 @@
+import Hero from "./_components/Hero";
+import ProductSection from "./_components/ProductSection";
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <ProductSection />
+    </main>
+  );
+}
